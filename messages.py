274965b3980e363a1377d74d6d866fd1805cfa97
Franklin Sarmiento <franklinitiel@gmail.com>
2021-03-13 10:23:46 -0500

    Global messages have been defined.

---
+++ messages.py
@@ -0,0 +1,7 @@
+class Messages:
+    """
+    This class allow to define all messages used in the script.
+    """
+    SUCCESS_MESSAGES = "Process Complete!. The least number is {0} with bouncy numbers' proportion equal to {1}%"
+    PERCENTAGE_INVALID_MESSAGE = "The percentage required to test should be greater zero"
+    PROCESSING_MESSAGE = "Processing with percentage: {0}%..."
