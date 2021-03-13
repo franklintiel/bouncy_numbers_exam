7a10eb6328737f505d27ed84a9c793ead3f9e5ed
Franklin Sarmiento <franklinitiel@gmail.com>
2021-03-13 10:23:57 -0500

    Global constants have been defined.

---
+++ constants.py
@@ -0,0 +1,10 @@
+class Constants:
+    """
+    This class allow to define all constant values used in the script.
+    This values constants can be modified in the next versions, this allow that script to be scalable
+    """
+    FIRST_BOUNCY_NUMBER = 101
+    MAX_PERCENTAGE_ALLOWED = 100
+    PERCENTAGE_REQUIRED = 99
+    PERCENTAGE_MINIMUM_REQUIRED = 0
+    DECIMALS_ALLOWED = 2
